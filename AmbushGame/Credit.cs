a44b471e0c206a9217cf987652f4d1e6e9c4e501
Stefan Sjöberg <bird.nerd@outlook.com>
2024-12-05 16:46:37 +0100

    Add project files.

---
+++ AmbushGame/Credit.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.Content.PM;
+
+namespace AmbushGame
+{
+    [Activity(Label = "Credit")]
+    public class Credit : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            SetContentView(Resource.Layout.Credit);
+            TextView appName = FindViewById<TextView>(Resource.Id.textViewAppName);
+            TextView versionName = FindViewById<TextView>(Resource.Id.textViewVersionName);
+            appName.Text = ApplicationInfo.LoadLabel(PackageManager);
+            versionName.Text = Application.Context.PackageManager.GetPackageInfo(Application.Context.PackageName, PackageInfoFlags.MetaData).VersionName.ToString();
+            //leaderboard.Adapter = new ArrayAdapter<int>(this, Android.Resource.Layout.SimpleListItem1, _player);
+        }
+    }
+}
---
+++ AmbushGame/MainActivity.cs
@@ -0,0 +1,90 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+using System;
+
+namespace AmbushGame
+{
+    [Activity(Label = "AmbushGame", MainLauncher = true)]
+    public class MainActivity : Activity
+    {
+        Button b1, b2, b3, retry, credit;
+        TextView scoreview;
+        static string ambush = new Random().Next(1, 4).ToString();
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+
+            int score = 0;
+
+            b1 = FindViewById<Button>(Resource.Id.button1);
+            b2 = FindViewById<Button>(Resource.Id.button2);
+            b3 = FindViewById<Button>(Resource.Id.button3);
+            retry = FindViewById<Button>(Resource.Id.buttonRetry);
+            credit = FindViewById<Button>(Resource.Id.buttonCredit);
+            scoreview = FindViewById<TextView>(Resource.Id.textViewScore);
+
+            void GameOver()
+            {
+                b1.Enabled = false;
+                b2.Enabled = false;
+                b3.Enabled = false;
+                retry.Visibility = Android.Views.ViewStates.Visible;
+            }
+
+            void ResetGame()
+            {
+                b1.Enabled = true;
+                b2.Enabled = true;
+                b3.Enabled = true;
+                score = 0;
+                ambush = new Random().Next(1, 4).ToString();
+                scoreview.Text = score.ToString();
+                retry.Visibility = Android.Views.ViewStates.Invisible;
+            }
+
+            b1.Click += AmbushEvent;
+            b2.Click += AmbushEvent;
+            b3.Click += AmbushEvent;
+
+            retry.Visibility = Android.Views.ViewStates.Invisible;
+
+            credit.Click += (o, e) =>
+            {
+                StartActivity(typeof(Credit));
+            };
+
+            retry.Click += (o, e) =>
+            {
+                ResetGame();
+            };
+
+            void AmbushEvent(object sender, EventArgs e)
+            {
+                Button b = sender as Button;
+
+                if (b.Text == ambush)
+                {
+                    GameOver();
+                    var gameOverDialog = new AlertDialog.Builder(this);
+                    gameOverDialog.SetCancelable(false);
+                    gameOverDialog.SetMessage("Här var det ett bakhåll.");
+                    gameOverDialog.SetNegativeButton("Stäng dialog", delegate { });
+                    gameOverDialog.SetTitle("Ajdå!");
+                    gameOverDialog.Show();
+                }
+                else
+                {
+                    score++;
+                    scoreview.Text = score.ToString();
+                    ambush = new Random().Next(1, 4).ToString();
+                }
+            }
+        }
+    }
+}
+
